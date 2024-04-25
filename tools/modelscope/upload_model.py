3a5d15bd722cfbac26441cb2f178fae437518819
moxying <moxyinggg@gmail.com>
2024-04-25 19:29:31 +0800

    add modelscope

---
+++ tools/modelscope/upload_model.py
@@ -0,0 +1,13 @@
+from modelscope.hub.api import HubApi
+import os
+
+MODELSCOPE_ACCESS_TOKEN = os.environ("MODELSCOPE_ACCESS_TOKEN")
+MODELSCOPE_MODEL_ID = os.environ("MODELSCOPE_MODEL_ID")
+MODELSCOPE_MODEL_DIR = os.environ("MODELSCOPE_MODEL_DIR")
+
+api = HubApi()
+api.login(MODELSCOPE_ACCESS_TOKEN)
+api.push_model(
+    model_id=MODELSCOPE_MODEL_ID,
+    model_dir=MODELSCOPE_MODEL_DIR,
+)
