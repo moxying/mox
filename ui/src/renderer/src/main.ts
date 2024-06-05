037c4aef8fccb3276ced728f251798e0ace40e1b
moxying <moxyinggg@gmail.com>
2024-06-05 16:28:26 +0800

    add color palettes

--- ui/src/renderer/src/main.ts
+++ ui/src/renderer/src/main.ts
@@ -1,19 +1,13 @@
+import '@/style/reset.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
 import i18n from '@/plugins/i18n'
+import vuetify from '@/plugins/vuetify'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-// 引入重置样式
-import '@/style/reset.scss'
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify, type ThemeDefinition } from 'vuetify'
-import { md3 } from 'vuetify/blueprints'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 // 导入公共样式
 import '@/style/common.scss'
 // ElementPlus
@@ -30,57 +24,6 @@ import 'dayjs/locale/zh-cn' // 导入本地化语言
 import relativeTime from 'dayjs/plugin/relativeTime' // 导入插件
 dayjs.locale('zh-cn') // 使用本地化语言
 dayjs.extend(relativeTime)
-
-// Vuetify
-const myCustomLightTheme: ThemeDefinition = {
-  dark: true,
-  colors: {
-    background: '#141218',
-    surface: '#141218',
-    'surface-bright': '#3B383E',
-    'surface-light': '#424242', // TODO
-    'surface-variant': '#49454F',
-    'on-surface-variant': '#CAC4D0',
-    primary: '#D0BCFE',
-    'primary-darken-1': '#277CC1', // TODO
-    secondary: '#CCC2DC',
-    'secondary-darken-1': '#48A9A6', // TODO
-    error: '#F2B8B5',
-    info: '#2196F3', // TODO
-    success: '#4CAF50', // TODO
-    warning: '#FB8C00' // TODO
-  },
-  variables: {
-    'border-color': '#FFFFFF',
-    'border-opacity': 0.12,
-    'high-emphasis-opacity': 1,
-    'medium-emphasis-opacity': 0.7,
-    'disabled-opacity': 0.5,
-    'idle-opacity': 0.1,
-    'hover-opacity': 0.04,
-    'focus-opacity': 0.12,
-    'selected-opacity': 0.08,
-    'activated-opacity': 0.12,
-    'pressed-opacity': 0.16,
-    'dragged-opacity': 0.08,
-    'theme-kbd': '#212529',
-    'theme-on-kbd': '#FFFFFF',
-    'theme-code': '#343434',
-    'theme-on-code': '#CCCCCC'
-  }
-}
-const vuetify = createVuetify({
-  ssr: true,
-  components,
-  directives,
-  blueprint: md3,
-  theme: {
-    defaultTheme: 'myCustomLightTheme',
-    themes: {
-      myCustomLightTheme
-    }
-  }
-})
 
 const app = createApp(App)
 
